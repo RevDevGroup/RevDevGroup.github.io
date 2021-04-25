f45b95d4ff72b557f9be0974d185aec6060bfdb4
JimScope <jimscope@protonmail.com>
2021-04-25 16:52:48 -0400

    Added Line- Awesome  an Google Analitycs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,36 @@
+// Toggle
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+
 anime({
   targets: '.circ-principal',
+  scale: { value: 1.1, duration: 3000, delay: 1300},
   translateY: { value: 30, duration: 5000 },
-  scale: { value: 1.05, duration: 3000, delay: 1300, easing: 'easeInOutQuint'},
   easing: 'easeInOutQuad',
   direction: 'alternate',
   loop: true,
 });
-
