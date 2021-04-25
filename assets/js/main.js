3b4e962e9ece1b698427d1fdcc296684b84d930a
JimScope <jimscope@protonmail.com>
2021-04-25 04:00:49 -0400

    Added AnimeJS for Animater elements

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+anime({
+  targets: '.circ-principal',
+  translateY: { value: 30, duration: 5000 },
+  scale: { value: 1.05, duration: 3000, delay: 1300, easing: 'easeInOutQuint'},
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+});
+
